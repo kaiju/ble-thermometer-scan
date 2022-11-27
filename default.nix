24bf924e30fd7bd0e66e15c86b65fb290fe0a19c
Josh <josh@mast.zone>
2022-11-26 20:50:46 -0500

    add name and version to derivation

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.poetry2nix.mkPoetryApplication {
+  name = "ble-thermometer-scan";
+  version = "0.2";
   projectDir = ./.;
   overrides = [
     pkgs.poetry2nix.defaultPoetryOverrides
