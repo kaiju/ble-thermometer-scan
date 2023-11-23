8b03f45df69cc577fe2e907c469379580e39a09e
Josh <josh@mast.zone>
2023-11-23 18:47:34 -0500

    use out-of-tree poetry2nix

--- default.nix
+++ default.nix
@@ -1,10 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.poetry2nix.mkPoetryApplication {
+let
+  poetry2nix = pkgs.callPackage (pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "poetry2nix";
+    rev = "2023.11.233109";
+    hash = "sha256-jOcL6cQhUGmnZT8KGHiwdEi7xSbdheb0MgDE8cf1rVM=";
+  }) {};
+in poetry2nix.mkPoetryApplication {
   name = "ble-thermometer-scan";
   version = "0.2";
   projectDir = ./.;
   overrides = [
-    pkgs.poetry2nix.defaultPoetryOverrides
+    poetry2nix.defaultPoetryOverrides
   ];
 }
 
