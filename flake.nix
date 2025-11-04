399df09f2c6550225c05945ec4f04ed0a34e9322
Josh <josh@mast.zone>
2025-11-03 20:49:50 -0500

    poetry -> uv

--- flake.nix
+++ flake.nix
@@ -2,25 +2,94 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: (
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages.ble-thermometer-scan = import ./default.nix { inherit pkgs; };
+
+        # uv workspace
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+        # workspace overlay that we'll apply to the python set
+        workspaceOverlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
+
+        # python set w/ our workspace overlay
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            python = pkgs.python313;
+          }).overrideScope
+            (
+              nixpkgs.lib.composeManyExtensions [
+                pyproject-build-systems.overlays.wheel
+                workspaceOverlay
+              ]
+            );
+      in
+      rec {
+
+        packages.ble-thermometer-scan = pythonSet.mkVirtualEnv "ble-thermometer-scan-env" workspace.deps.default;
         defaultPackage = packages.ble-thermometer-scan;
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.poetry ];
-          inputsFrom = builtins.attrValues self.packages.${system};
-        };
+
+        devShell =
+          let
+            editableOverlay = workspace.mkEditablePyprojectOverlay {
+              root = "$REPO_ROOT";
+            };
+            editablePythonset = pythonSet.overrideScope editableOverlay;
+            virtualenv = editablePythonset.mkVirtualEnv "ble-thermometer-scan-dev-env" workspace.deps.all;
+          in
+          pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+            ];
+
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = editablePythonset.python.interpreter;
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+
+            shellHook = ''
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+            '';
+          };
+
       }
-    )
-  ) // {
+    ))
+    // {
 
-    overlay = (final: prev: { ble-thermometer-scan = self.packages.${prev.system}.ble-thermometer-scan; });
+      overlay = (
+        final: prev: { ble-thermometer-scan = self.packages.${prev.system}.ble-thermometer-scan; }
+      );
 
-  };
+    };
 
 }
